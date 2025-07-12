1229d5d10223e9294b1f4f21ce1f43390a723fea
Mars Adeniji <adenijifara@gmail.com>
2025-07-12 15:50:31 +0300

    Finish first day

---
+++ day_1-band_name_generator/README.md
@@ -0,0 +1,6 @@
+# Band Name generator
+Generates a band name using
+- Name of city born
+- Name of a pet
+
+Combines both to make a band name ✨
---
+++ day_1-band_name_generator/app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+user_city = input("What's the name of the city you grew up in?\n")
+user_pet = input("What's the name of your pet?\n")
+band_name = f"{user_city} {user_pet}"
+print(f"Your band name could be: {band_name}")
