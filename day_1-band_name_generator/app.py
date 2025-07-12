5aecf6f412260818fe3ab4093dc189d974bb8cde
Mars Adeniji <adenijifara@gmail.com>
2025-07-12 15:51:23 +0300

    Add greeting

--- day_1-band_name_generator/app.py
+++ day_1-band_name_generator/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+print("Welcome to the Band Name Generator!")
 user_city = input("What's the name of the city you grew up in?\n")
 user_pet = input("What's the name of your pet?\n")
 band_name = f"{user_city} {user_pet}"
