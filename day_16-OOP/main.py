1727f3dcf381665d8e89c942611b41c4967c378b
Mars Adeniji <adenijifara@gmail.com>
2025-08-04 04:02:17 +0300

    Initial commit

---
+++ day_16-OOP/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
