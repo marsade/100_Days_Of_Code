283b47e5ce74da77a8d81366b22158d0994d093f
Mars Adeniji <adenijifara@gmail.com>
2025-09-10 22:35:07 +0300

    Add additional files, skipping

--- day_16-OOP/main.py
+++ day_16-OOP/main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-from turtle import Turtle, Screen
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+espresso = MenuItem("espresso", 50, 0, 18, 1.5)
+latte = MenuItem("latte", 100, 150, 24, 2.5)
+cappuccino = MenuItem("cappuccino", 250, 100, 24, 3.0)
+
+choice = input("What would you like to order? (espresso/latte/cappuccino): ")
+
+if choice == "espresso":
+    maker = CoffeeMaker()
+    print(maker.is_resource_sufficient(espresso))
