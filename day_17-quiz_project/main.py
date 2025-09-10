9e2cd278dedb37cd6b66f3dda2c44fd3c0431d97
Mars Adeniji <adenijifara@gmail.com>
2025-09-11 02:11:24 +0300

    Add final score alert

--- day_17-quiz_project/main.py
+++ day_17-quiz_project/main.py
@@ -13,3 +13,6 @@ new_quiz = QuizBrain(question_bank)
 
 while new_quiz.still_has_questions():
     new_quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was {new_quiz.score} / {new_quiz.question_number}")
