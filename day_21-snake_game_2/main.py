95727b252d70ae7ab5346a8891c842e0e91bd440
Fara Ade <adenijifara@gmail.com>
2026-01-22 14:30:03 +0200

    Add food collision and scoreboard update

--- day_21-snake_game_2/main.py
+++ day_21-snake_game_2/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 scr = Screen()
@@ -10,6 +12,8 @@ scr.title("Snake Game")
 scr.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Scoreboard()
 
 scr.listen()
 scr.onkey(key="Up", fun=snake.up)
@@ -19,8 +23,14 @@ scr.onkey(key="Right", fun=snake.right)
 
 game_is_on = True
 while game_is_on:
-    scr.update() 
+    scr.update()
     time.sleep(0.2)
 
+
     snake.move()
+    
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increment()
 scr.exitonclick()
