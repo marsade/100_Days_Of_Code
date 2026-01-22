9ea9936797232e1b0d59f0b633b77ddc579e9210
Fara Ade <adenijifara@gmail.com>
2026-01-22 21:18:54 +0200

    Use slicing in tail collision

--- day_21-snake_game_2/main.py
+++ day_21-snake_game_2/main.py
@@ -31,10 +31,17 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         score.increment()
+        snake.extend()
 
     # Detect collision with wall.
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or \
        snake.head.ycor() > 300 or snake.head.ycor() < -300:
-        score.game_over()
         game_is_on = False
+        score.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 scr.exitonclick()
