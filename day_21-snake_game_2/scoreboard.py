f1aca50dc2f7b80b1a69a21bdeb1993dfa303d8f
Fara Ade <adenijifara@gmail.com>
2026-01-22 15:30:44 +0200

    Add constants and game over method

--- day_21-snake_game_2/scoreboard.py
+++ day_21-snake_game_2/scoreboard.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 '''Scoreboard class'''
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "bold")
 
 
 class Scoreboard(Turtle):
     score = 0
+
     def __init__(self):
         super().__init__()
-        self.goto(0, 280)
+        self.goto(0, 270)
         self.color("white")
-        self.write(f'Score: {self.score}', align="center", font=('Arial', 11, 'bold'))
+        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
         self.hideturtle()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increment(self):
         self.score += 1
         self.clear()
-        self.write(f'Score: {self.score}', align="center", font=('Arial', 12, 'bold'))
+        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
