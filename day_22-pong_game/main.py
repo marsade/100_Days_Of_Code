528c2b1e38ac860f5228bd58167b05840b8f6a82
Fara Ade <adenijifara@gmail.com>
2026-01-25 18:14:28 +0200

    add ball instance

--- day_22-pong_game/main.py
+++ day_22-pong_game/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import time
-from turtle import Screen, Turtle
-
+from ball import Ball
+from turtle import Screen
+from paddle import Paddle
 
 scr = Screen()
 scr.setup(width=800, height=600)
@@ -9,31 +10,20 @@ scr.bgcolor("black")
 scr.title("Pong")
 scr.tracer(0)
 
-user = Turtle("square")
-user.shapesize(stretch_wid=100/20, stretch_len=20/20)
-user.speed("fastest")
-user.color("white")
-user.penup()
-user.goto(350, 0)
-
-
-def move_up():
-    if user.ycor() < 242:
-        user.sety(user.ycor() + 20)
-
-
-def move_down():
-    if user.ycor() > -240:
-        user.sety(user.ycor() - 20)
-
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
 
 scr.listen()
-scr.onkey(fun=move_up, key="Up")
-scr.onkey(fun=move_down, key="Down")
+scr.onkey(fun=r_paddle.move_up, key="Up")
+scr.onkey(fun=r_paddle.move_down, key="Down")
 
+scr.onkey(fun=l_paddle.move_up, key="w")
+scr.onkey(fun=l_paddle.move_down, key="s")
 game_is_on = True
 while game_is_on:
     scr.update()
-    time.sleep(0.2)
+    time.sleep(0.3)
+    ball.move()
 
 scr.exitonclick()
