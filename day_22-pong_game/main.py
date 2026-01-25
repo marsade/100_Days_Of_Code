1bc37031256975f8ca8a2ee8e60dbedc266346f3
Fara Ade <adenijifara@gmail.com>
2026-01-25 13:43:58 +0200

    Create paddle with listeners

--- day_22-pong_game/main.py
+++ day_22-pong_game/main.py
@@ -7,18 +7,23 @@ scr.setup(width=800, height=600)
 scr.bgcolor("black")
 scr.title("Pong")
 
-user = Turtle()
+user = Turtle("square")
+user.shapesize(stretch_wid=100/20, stretch_len=20/20)
 user.speed("fastest")
-user.hideturtle()
 user.color("white")
 user.penup()
 user.goto(350, 0)
-user.pendown()
-user.setheading(0)
-user.begin_fill()
-for dim in [20, 100, 20, 100]:
-    user.forward(dim)
-    user.right(90)
-user.end_fill()
+
+def move_up():
+    if user.ycor() < 242:
+        user.sety(user.ycor() + 20)
+
+def move_down():
+    if user.ycor() > -240:
+        user.sety(user.ycor() - 20)
+
+scr.listen()
+scr.onkey(fun=move_up, key="Up")
+scr.onkey(fun=move_down, key="Down")
 
 scr.exitonclick()
