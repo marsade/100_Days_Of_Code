3a1c3e52e464d824956913e09ea26cfc5c93e709
Fara Ade <adenijifara@gmail.com>
2026-01-25 22:33:53 +0200

    add bounce method

--- day_22-pong_game/ball.py
+++ day_22-pong_game/ball.py
@@ -9,7 +9,13 @@ class Ball(Turtle):
         self.color("white")
         self.speed("fastest")
         self.penup()
-        self.left(30)
+        self.y_move = 10
+        self.x_move = 10
 
     def move(self):
-        self.forward(20)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
