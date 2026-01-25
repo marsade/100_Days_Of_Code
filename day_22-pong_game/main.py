12636e4f6dbc66b2b7bde60294733f6b7a2c81e2
Fara Ade <adenijifara@gmail.com>
2026-01-25 13:48:10 +0200

    add animation update and clean up code

--- day_22-pong_game/main.py
+++ day_22-pong_game/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import time
 from turtle import Screen, Turtle
 
 
@@ -6,6 +7,7 @@ scr = Screen()
 scr.setup(width=800, height=600)
 scr.bgcolor("black")
 scr.title("Pong")
+scr.tracer(0)
 
 user = Turtle("square")
 user.shapesize(stretch_wid=100/20, stretch_len=20/20)
@@ -14,16 +16,24 @@ user.color("white")
 user.penup()
 user.goto(350, 0)
 
+
 def move_up():
     if user.ycor() < 242:
         user.sety(user.ycor() + 20)
+
 
 def move_down():
     if user.ycor() > -240:
         user.sety(user.ycor() - 20)
 
+
 scr.listen()
 scr.onkey(fun=move_up, key="Up")
 scr.onkey(fun=move_down, key="Down")
+
+game_is_on = True
+while game_is_on:
+    scr.update()
+    time.sleep(0.2)
 
 scr.exitonclick()
