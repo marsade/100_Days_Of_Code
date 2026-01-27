c2141ad4c986ad7c47805d32ea85bcff2b83f9ca
Fara Ade <adenijifara@gmail.com>
2026-01-27 19:13:16 +0200

    clean up code

--- day_22-pong_game/main.py
+++ day_22-pong_game/main.py
@@ -3,6 +3,7 @@ import time
 from ball import Ball
 from turtle import Screen
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 scr = Screen()
 scr.setup(width=800, height=600)
@@ -13,6 +14,7 @@ scr.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 ball = Ball()
+score = Scoreboard()
 
 scr.listen()
 scr.onkey(fun=r_paddle.move_up, key="Up")
@@ -27,20 +29,24 @@ while game_is_on:
 
     ball.move()
 
-    #Detect collision with walls
+    # Detect collision with walls
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    #Detect collision with paddles
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
+    # Detect collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > \
+       330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
         ball.bounce_x()
+        ball.incr_speed()
 
-    #Detect out of bounds(r_paddle)
+    # Detect out of bounds(r_paddle)
     if ball.xcor() > 380:
         ball.reset_position()
+        score.l_point()
 
-    #Detect out of bounds(l_paddle)
+    # Detect out of bounds(l_paddle)
     if ball.xcor() < -380:
         ball.reset_position()
+        score.r_point()
 
 scr.exitonclick()
