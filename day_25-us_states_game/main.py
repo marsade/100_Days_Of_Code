a4630c1dcef6faee5988b136e647599067a7e30a
Fara Ade <adenijifara@gmail.com>
2026-04-07 14:31:28 +0300

    Initial commit

---
+++ day_25-us_states_game/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import csv
+
+
+# Opening without csv
+# with open("weather_data.csv") as f:
+#     data = f.readlines()
+
+# Opening with csv
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
