1168ab554b2df2d6cb5f303b51cc74e1219e9ce2
Fara Ade <adenijifara@gmail.com>
2026-04-16 12:57:37 +0300

    Add exit condition

--- day_25-us_states_game/main.py
+++ day_25-us_states_game/main.py
@@ -11,6 +11,37 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+df = pandas.read_csv("./50_states.csv")
+right_guesses = []
+score = 0
+
+all_states = df.state.to_list()
+
+while len(right_guesses) < 50:
+    answer_state = screen.textinput(title=f"{score}/50 States Correct",
+                                    prompt="What's another state's name?")
+    usr_ans = answer_state.title()
+
+    if usr_ans == "Exit":
+        for state in right_guesses:
+            if state in all_states:
+                all_states.remove(state)
+
+        states_to_learn = pandas.Series(all_states)
+        states_to_learn.to_csv("states_to_learn.csv", index=False)
+        break
+
+    if usr_ans in all_states and usr_ans not in right_guesses:
+        score += 1
+        turt = turtle.Turtle()
+        turt.penup()
+        turt.hideturtle()
+        x_coor = df[df.state == usr_ans].x.values[0]
+        y_coor = df[df.state == usr_ans].y.values[0]
+        turt.goto(x_coor, y_coor)
+        turt.pendown()
+        turt.write(usr_ans, font=("Arial", 10, "normal"))
+        right_guesses.append(usr_ans)
+
 
 turtle.mainloop()
