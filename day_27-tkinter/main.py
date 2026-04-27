aa271a55d4465aad9199d23008c62500243b11e5
Fara Ade <adenijifara@gmail.com>
2026-04-27 13:19:17 +0300

    Create Labels, Buttons, Entry

--- day_27-tkinter/main.py
+++ day_27-tkinter/main.py
@@ -9,4 +9,20 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a Label", font= ("Arial", 24, "bold"))
 my_label.pack()
 
+my_label["text"] = "New Text"
+
+#Buttons
+def button_clicked():
+    print("I got clicked")
+    my_label["text"] = input.get()
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+#Entry
+
+input = tkinter.Entry()
+input.pack()
+
 window.mainloop()
