c19b8f67d86b91ff1f2e8d5c92154e5047c635b8
Fara Ade <adenijifara@gmail.com>
2026-05-03 01:28:28 +0300

    Create timer mechanism and app UI

--- day_28-pomodoro_app/main.py
+++ day_28-pomodoro_app/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,8 +14,42 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+header = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
+header.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="./tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 138, text="0:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", bg=YELLOW, bd=0, activebackground=YELLOW, highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", bg=YELLOW, bd=0, highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+tick_label = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 25, "bold"))
+tick_label.grid(column=1, row=3)
+
+
+window.mainloop()
