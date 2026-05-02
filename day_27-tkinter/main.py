6e104156c771fb6a8577edcedc2649426c3b9244
Fara Ade <adenijifara@gmail.com>
2026-05-02 22:52:56 +0300

    Add grid layout and padding

--- day_27-tkinter/main.py
+++ day_27-tkinter/main.py
@@ -4,11 +4,12 @@ import tkinter
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 #Label
 my_label = tkinter.Label(text="I am a Label", font= ("Arial", 24, "bold"))
 my_label["text"] = "New Text"
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 #Buttons
 def button_clicked():
@@ -17,11 +18,13 @@ def button_clicked():
 
 
 button = tkinter.Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+button2 = tkinter.Button(text="New Button")
+button2.grid(column=2, row=0)
 
 #Entry
-
 input = tkinter.Entry()
-input.pack()
+input.grid(column=3, row=2)
 
 window.mainloop()
