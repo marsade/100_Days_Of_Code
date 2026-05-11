f9c88197b39abfe5f8e4f5360f6d27956f8d86b7
Fara Ade <adenijifara@gmail.com>
2026-05-11 16:06:15 +0300

    Split geometry managers to new line

--- day_29-password_manager_gui/main.py
+++ day_29-password_manager_gui/main.py
@@ -20,17 +20,26 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 #Labels
-web_label = Label(text="Website:").grid(row=1, column=0)
-mail_label = Label(text="Email/Username:").grid(row=2, column=0)
-pass_label = Label(text="Password:").grid(row=3, column=0)
+web_label = Label(text="Website:")
+web_label.grid(row=1, column=0)
+mail_label = Label(text="Email/Username:")
+mail_label.grid(row=2, column=0)
+pass_label = Label(text="Password:")
+pass_label.grid(row=3, column=0)
 
 #Entries
-web_entry = Entry(width=35).grid(row=1, column=1, columnspan=2)
-mail_entry = Entry(width=35).grid(row=2, column=1, columnspan=2)
-pwd_entry = Entry(width=20).grid(row=3, column=1)
+web_entry = Entry(width=35)
+web_entry.grid(row=1, column=1, columnspan=2)
+web_entry.focus()
+mail_entry = Entry(width=35)
+mail_entry.grid(row=2, column=1, columnspan=2)
+pwd_entry = Entry(width=35)
+pwd_entry.grid(row=3, column=1)
 
 #Buttons
-gen_btn = Button(text="Generate Password").grid(row=3, column=2)
-add_btn = Button(text="Add", width=36).grid(row=4, column=1)
+gen_btn = Button(text="Generate Password")
+gen_btn.grid(row=3, column=2)
+add_btn = Button(text="Add", width=36)
+add_btn.grid(row=4, column=1)
 
 window.mainloop()
