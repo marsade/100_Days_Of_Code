a55b307cd44c8fd0cf4506dee8f1ba00beb99d27
Fara Ade <adenijifara@gmail.com>
2026-06-03 13:51:46 +0300

    Add validation and dialog warnings

--- day_29-password_manager_gui/main.py
+++ day_29-password_manager_gui/main.py
@@ -3,10 +3,30 @@
 
 
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    web_inp = web_entry.get()
+    mail_inp = mail_entry.get()
+    pass_inp = pwd_entry.get()
+
+    if web_inp == "" or pass_inp == "":
+        messagebox.showwarning(title="Oops", message="Please do not leave any fields empty")
+    else:
+        is_ok = messagebox.askokcancel(title=web_inp, message=f"These are the details entered: \nEmail: {mail_inp} \nPassword: {pass_inp} \nIs it okay to save?")
+
+        if is_ok:    
+            line = f"{web_inp} | {mail_inp} | {pass_inp}\n"
+
+            with open("data.txt", "a") as f:
+                f.write(line)
+
+            web_entry.delete(0, END)
+            # mail_entry.delete(0, END)
+            pwd_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -33,13 +53,14 @@ web_entry.grid(row=1, column=1, columnspan=2)
 web_entry.focus()
 mail_entry = Entry(width=35)
 mail_entry.grid(row=2, column=1, columnspan=2)
+mail_entry.insert(0, "adenijifara@gmail.com")
 pwd_entry = Entry(width=35)
 pwd_entry.grid(row=3, column=1)
 
 #Buttons
 gen_btn = Button(text="Generate Password")
 gen_btn.grid(row=3, column=2)
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=save)
 add_btn.grid(row=4, column=1)
 
 window.mainloop()
