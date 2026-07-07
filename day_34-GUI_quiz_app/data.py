cfcb4da28a5007081f5ec521197766af414f1382
Fara Ade <adenijifara@gmail.com>
2026-07-07 20:14:42 +0300

    Refactor to get questions from API

---
+++ day_34-GUI_quiz_app/data.py
@@ -0,0 +1,13 @@
+#!/use/bin/env python3
+import requests
+
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+
+question_data = response.json()["results"]
