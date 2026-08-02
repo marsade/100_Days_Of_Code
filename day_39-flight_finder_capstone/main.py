d61d816fef2ecd5d4cff3982a6312b1a5a873e74
Fara Ade <adenijifara@gmail.com>
2026-08-03 02:34:55 +0300

    Add request cache and import modules

--- day_39-flight_finder_capstone/main.py
+++ day_39-flight_finder_capstone/main.py
@@ -1,1 +1,22 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+#!/usr/bin/env python3
+import os
+import requests_cache
+from data_manager import DataManager
+from datetime import datetime
+from dotenv import load_dotenv
+from flight_search import FlightSearch
+from pprint import pprint
+
+
+load_dotenv()
+requests_cache.install_cache(
+    "flight_cache",
+    expire_after=3600,
+    urls_expire_after={
+        os.getenv("SHEET_URL"): requests_cache.DO_NOT_CACHE
+    }
+)
+
+search_flight = FlightSearch()
+sheet_data = DataManager()
+pprint(sheet_data.get_prices())
