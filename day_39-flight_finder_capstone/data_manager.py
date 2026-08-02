db1d55ffeca497fe5f2a62fe4c5b80731afdbc0f
Fara Ade <adenijifara@gmail.com>
2026-08-03 02:30:19 +0300

    Add get_prices method

--- day_39-flight_finder_capstone/data_manager.py
+++ day_39-flight_finder_capstone/data_manager.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.sheety_url = os.getenv("SHEET_URL")
+        self.headers = {
+            "Authorization": f"Basic {os.getenv("SHEETY_TOKEN")}"
+        }
+
+    def get_prices(self):
+        self.res = requests.get(self.sheety_url, headers=self.headers)
+        return(self.res.json()["prices"])
