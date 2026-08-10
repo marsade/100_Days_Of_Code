915c5ef685c8691a9cf7c55f463a19bf03238922
Fara Ade <adenijifara@gmail.com>
2026-08-10 23:08:01 +0300

    Setup class

--- day_39-flight_finder_capstone/notification_manager.py
+++ day_39-flight_finder_capstone/notification_manager.py
@@ -1,3 +1,32 @@
+#!/usr/bin/env python3
+'''Twilio Notification Manager Class'''
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+
+load_dotenv()
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self, fl_price, dep, arr, out, in_):
+        self.acc_sid = os.getenv("TW_ACC_SID")
+        self.auth = os.getenv("TW_AUTH")
+        self.client = Client(self.acc_sid, self.auth)
+        self.msg_body = f"Low price alert! Only ${fl_price} to fly from {dep} to {arr}, on {out} until {in_}"
+
+    def send_sms(self):
+        message = self.client.messages \
+            .create(
+                body=self.msg_body,
+                from_=os.getenv("TW_VIRTUAL_NUM"),
+                to=os.getenv("TW_VERIFIED_NUM")
+            )
+        print(message.status)
+
+    def send_whatsapp(self):
+        message = self.client.messages \
+            .create(
+                body=self.msg_body,
+                from_=f"whatsapp:{os.getenv("TW_VIRWP_NUM")}",
+                to=f"whatsapp:{os.getenv("TW_WHATSAPP_NUM")}"
+            )
+        print(message.sid)
