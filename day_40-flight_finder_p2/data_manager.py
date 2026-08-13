c4d14b092d749773583e3112e93f3d31160a8282
Fara Ade <adenijifara@gmail.com>
2026-08-13 21:23:18 +0300

    feat: add method to fetch user emails from sheety

--- day_40-flight_finder_p2/data_manager.py
+++ day_40-flight_finder_p2/data_manager.py
@@ -8,17 +8,22 @@ load_dotenv()
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.sheety_url = os.getenv("SHEET_URL")
+        self.sheety_price_url = os.getenv("SHEETY_PRICE_URL")
+        self.sheety_users_url = os.getenv("SHEETY_USERS_URL")
         self.headers = {
             "Authorization": f"Basic {os.getenv("SHEETY_TOKEN")}"
         }
 
     def get_prices(self):
-        res = requests.get(self.sheety_url, headers=self.headers)
+        res = requests.get(self.sheety_price_url, headers=self.headers)
         return(res.json()["prices"])
 
+    def get_customer_email(self):
+        res = requests.get(self.sheety_users_url, headers=self.headers)
+        return(res.json()["users"])
+
     def update_lowest_price(self, row_id, new_price):
-        self.put_url = f"{self.sheety_url}/{row_id}"
+        self.put_url = f"{self.sheety_price_url}/{row_id}"
 
         payload = {
             "price": {
