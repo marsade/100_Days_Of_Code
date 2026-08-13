9192c4d9f20242d1613515a3ecd0bf60c40d53b8
Fara Ade <adenijifara@gmail.com>
2026-08-13 18:44:38 +0300

    Add stops var to __init__

--- day_40-flight_finder_p2/flight_data.py
+++ day_40-flight_finder_p2/flight_data.py
@@ -3,12 +3,13 @@
 
 
 class FlightData:
-    def __init__(self, price, origin_airport, destination_airport, out_date, return_date):
+    def __init__(self, price, origin_airport, destination_airport, out_date, return_date, stops):
         self.price = price
         self.origin_airport = origin_airport
         self.destination_airport = destination_airport
         self.out_date = out_date
         self.return_date = return_date
+        self.stops = stops
 
 
 def find_cheapest_flight(data, return_date):
@@ -19,7 +20,8 @@ def find_cheapest_flight(data, return_date):
             origin_airport="N/A",
             destination_airport="N/A",
             out_date="N/A",
-            return_date="N/A"
+            return_date="N/A",
+            stops="N/A"
         )]
         return fldata_list[0]
     fldata_list = [FlightData(
@@ -27,7 +29,8 @@ def find_cheapest_flight(data, return_date):
             origin_airport=flight["flights"][0]["departure_airport"]["id"],
             destination_airport=flight["flights"][-1]["arrival_airport"]["id"],
             out_date=flight["flights"][0]["departure_airport"]["time"],
-            return_date=return_date
+            return_date=return_date,
+            stops=len(flight["flights"]) - 1
         ) for flight in all_flights]
     sorted_flights = sorted(fldata_list, key=lambda f:f.price)
     return(sorted_flights[0])
