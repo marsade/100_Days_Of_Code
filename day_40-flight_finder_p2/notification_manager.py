11f8905adee27e56b70d19ea23919b9677721255
Fara Ade <adenijifara@gmail.com>
2026-08-13 21:20:54 +0300

    feat: add email alert notifications

--- day_40-flight_finder_p2/notification_manager.py
+++ day_40-flight_finder_p2/notification_manager.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 '''Twilio Notification Manager Class'''
 import os
+import smtplib
 from dotenv import load_dotenv
 from twilio.rest import Client
 
 
 load_dotenv()
 class NotificationManager:
-    def __init__(self, fl_price, dep, arr, out, in_):
+    def __init__(self, fl_price, dep, arr, out, in_, stops):
         self.acc_sid = os.getenv("TW_ACC_SID")
         self.auth = os.getenv("TW_AUTH")
         self.client = Client(self.acc_sid, self.auth)
-        self.msg_body = f"Low price alert! Only ${fl_price} to fly from {dep} to {arr}, on {out} until {in_}"
+        self.mail = os.getenv("SMTP_ADDRESS")
+        self.ml_psw = os.getenv("SMTP_PASSWORD")
+        if stops > 1:
+            self.msg_body = f"Low price alert! Only USD{fl_price} to fly from {dep} to {arr}, with {stops} stops departing on {out} and returning {in_}"
+        else:
+            self.msg_body = f"Low price alert! Only USD{fl_price} to fly directly from {dep} to {arr}, departing on {out} and returning {in_}"
 
     def send_sms(self):
         message = self.client.messages \
@@ -30,3 +36,13 @@ class NotificationManager:
                 to=f"whatsapp:{os.getenv("TW_WHATSAPP_NUM")}"
             )
         print(message.sid)
+
+    def send_emails(self, to_addr):
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=self.mail, password=self.ml_psw)
+            connection.sendmail(
+                from_addr=self.mail,
+                to_addrs= to_addr,
+                msg=self.msg_body
+            )
